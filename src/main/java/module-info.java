2fd329dfa06712cce10e952b0d4352c4b21b1685
Your Name <you@example.com>
2023-06-02 22:54:59 +0800

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module sample {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.xml;
+    opens ernesto.apksigner to javafx.fxml;
+
+    exports ernesto.apksigner;
+}
